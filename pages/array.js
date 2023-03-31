144958aff97c2235f9434fe05e06f9926014a92c
Joao Pedro <jpfox.alves@gmail.com>
2023-03-30 21:51:19 -0300

    aula dia 30-3

---
+++ pages/array.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Cabecalho from '../components/Cabecalho'
+import Rodape from '../components/Rodape'
+
+const array = () => {
+  return (
+    <>
+    <Cabecalho />
+    <h1>Array</h1>
+    <Rodape />
+    </>
+  )
+}
+
+export default array
---
+++ pages/pagina2.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Cabecalho from '../components/Cabecalho'
+import Pagina from '../components/Pagina'
+import Rodape from '../components/Rodape'
+
+const pagina2 = () => {
+  return (
+    <>
+      <Pagina />
+      <Container>
+      <h1 className=''>Pagina 2</h1>  
+      
+    </Container>
+
+    </>
+  )
+}
+
+export default pagina2
